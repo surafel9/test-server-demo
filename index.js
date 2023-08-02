d0fb37a04fe761b46ae618e0a401b0be0eb832f0
surafel <picolo.sur@gmail.com>
2023-08-01 20:41:46 -0700

    test server set up inital phase'
    '

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+// parse request bodies (req.body)
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`);
+});
